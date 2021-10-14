fad435b4b9124738414b80cb17dea941d3d63b76
Andrew Pellum <apellum19@gmail.com>
2021-10-14 11:24:00 -0600

    add repo for backend

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "21-22 Season Pass", category: "Season Pass", price: 800.00)
+Product.create(name: "Lift Ticket", category: "Lift Ticket", price: 100.00)
+Product.create(name: "21-22 10 Day Season Pass", category: "Season Pass", price: 500.00)
+Product.create(name: "Lift Ticket - 1/2 Day", category: "Lift Ticket", price: 60.00)
