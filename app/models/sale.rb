eb845c4d091ccfe81012a127a666646c927faf14
Andrew Pellum <apellum19@gmail.com>
2021-10-27 15:37:21 -0600

    Added dependent for delete

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,6 +1,6 @@
 class Sale < ApplicationRecord
   belongs_to :user
   belongs_to :customer
-  has_many :sale_products
+  has_many :sale_products, dependent: :destroy
   has_many :products, through: :sale_products
 end
